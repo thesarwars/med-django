1010a0c62a027ccde3a285191cb6a48915bd18bd
sarwars <alltime.id.master@gmail.com>
2023-05-28 02:28:01 +0600

    Create, Update, Variant View and URL Design Completed

--- mediustest/taha_app/urls.py
+++ mediustest/taha_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    
+    path('create/', ProductsView.as_view()),
+    path('update/<int:pk>', ProductUpdate.as_view()),
+    path('product-image/', ProductImageView.as_view()),
+    path('variant/', VariantView.as_view()),
 ]
  
--- mediustest/taha_app/views.py
+++ mediustest/taha_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from .serializers import *
+from .models import *
+from rest_framework import generics
+from django.http import JsonResponse
 # Create your views here.
+
+# Products create view
+class ProductsView(generics.ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    
+# Products Edit, Delete View    
+class ProductUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    
+    
+    
+# Products Image view
+class ProductImageView(generics.ListCreateAPIView):
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
+    
+# Variants create view
+class VariantView(generics.ListCreateAPIView):
+    queryset = Variant.objects.all()
+    serializer_class = VariantSerializer
+    
+    
